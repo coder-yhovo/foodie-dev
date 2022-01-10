e042fd9fe939e0e9f613876ac7c413549dfae8c9
yh <2047434067@qq.com>
2022-01-10 13:42:03 +0800

    工程整合

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.imooc.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
