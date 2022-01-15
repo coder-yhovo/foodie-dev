4cf5eb742bbb0a4ec281f06a71768cb67b691279
yh <2047434067@qq.com>
2022-01-15 14:39:52 +0800

    商品推荐

--- foodie-dev-mapper/src/main/java/com/imooc/mapper/CategoryMapperCustom.java
+++ foodie-dev-mapper/src/main/java/com/imooc/mapper/CategoryMapperCustom.java
@@ -2,9 +2,13 @@ package com.imooc.mapper;
 
 import com.imooc.my.mapper.MyMapper;
 import com.imooc.pojo.Category;
+import com.imooc.pojo.vo.NewItemsVO;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface CategoryMapperCustom {
     public List getSubCatList(Integer rootCatId);
+    public List<NewItemsVO> getSixNewItemsLazy(@Param("paramsMap") Map<String, Object> map);
 }
